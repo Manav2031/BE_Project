69613915eb7de94c3ab5fa6c9b19aec11a8712cb
Manav2031 <manavmehta2031@gmail.com>
2025-03-29 01:10:09 +0530

    Updated Code

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const app = express();
 
 const corsOptions = {
   origin: '*',
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
