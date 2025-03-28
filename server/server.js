20522a40037fe41c5bd0a865bfa98de5b4d042be
Manav2031 <manavmehta2031@gmail.com>
2025-03-28 15:45:01 +0530

    Updated Code

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const macAddressRoutes = require('./routes/macAddressRoutes');
-const cors = require("cors");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-const connectDb = require("./config/connectDb");
-const userRoute=require('./routes/userRoute');
-const usermacRoute=require('./routes/usermacRoute');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const connectDb = require('./config/connectDb');
+const userRoute = require('./routes/userRoute');
+const usermacRoute = require('./routes/usermacRoute');
 
 //config dotenv file
 dotenv.config();
@@ -17,16 +17,18 @@ connectDb();
 const app = express();
 
 const corsOptions = {
-  origin: '*'
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 //middlewares
-app.use(cors(corsOptions))
-app.use(morgan("dev"));
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api', macAddressRoutes);
-app.use('/api',userRoute);
-app.use('/api',usermacRoute);
+app.use('/api', userRoute);
+app.use('/api', usermacRoute);
 
 //port
 const PORT = process.env.PORT || 8080;
